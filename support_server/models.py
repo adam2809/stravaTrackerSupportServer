8d79372aa583391bf6834b9add22478c918a57e8
Adam Kulesza <adam.kule@gmail.com>
2021-03-03 18:55:02 +0100

    Added user table

--- support_server/models.py
+++ support_server/models.py
@@ -4,4 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    
+    refresh_token = models.CharField(max_length=50)
+    access_token = models.CharField(max_length=50)
+    esp_code = models.CharField(max_length=50)
+
