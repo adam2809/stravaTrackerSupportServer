541445170d2bab1946b676b28271714fbbbcc369
Adam Kulesza <adam.kule@gmail.com>
2021-03-03 19:40:06 +0100

    Added strava id column

--- support_server/models.py
+++ support_server/models.py
@@ -6,12 +6,15 @@ from django.db import models
 class User(models.Model):
     refresh_token = models.CharField(max_length=50)
     access_token = models.CharField(max_length=50)
-    esp_code = models.CharField(max_length=50)
+    esp_code = models.CharField(max_length=8)
+    strava_id = models.PositiveIntegerField(null=True)
 
     def __str__(self):
         return f'''{{
+            id: {self.id}
             refresh_token: {self.refresh_token}
             access_token: {self.access_token}
             esp_code: {self.esp_code}
+            strava_id: {self.strava_id}
         }}'''
 
